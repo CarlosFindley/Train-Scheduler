f2af6faf92e92af51a3ca830f152cb7866fbe392
Carlos Villalta <CarlosFindley@protonmail.com>
2018-11-03 22:16:49 -0700

    made the next arrival and minutes away features work

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -41,7 +41,7 @@ $("#submitBtn").on("click", function(event) {
   console.log(newTrain.time);
   console.log(newTrain.frequency);
 
-  alert("Train successfully added");
+//   alert("Train successfully added");
 
   // Clears all of the text-boxes
   $("#trainNameInput").val("");
@@ -90,19 +90,80 @@ database.ref().on("child_added", function(childSnapshot) {
     console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
     // Next Train
-    var nextTrainArrival = moment().add(tMinutesTillTrain, "minutes");
+    var nextTrainArrival = moment().add(tMinutesTillTrain, "minutes").format("LT");
     console.log("ARRIVAL TIME: " + moment(nextTrainArrival).format("HH:mm"));
   
     // Create the new row and add content to page
     var newRow = $("<tr>").append(
       $("<td>").text(trainName),
       $("<td>").text(trainDestination),
-      $("<td>").text(nextTrainArrival),
       $("<td>").text(trainFrequency),
+      $("<td>").text(trainTime),
+      $("<td>").text(nextTrainArrival),
       $("<td>").text(tMinutesTillTrain),
     );
   
     // Append the new row to the table
     $("#trainTable > tbody").append(newRow);
   });
+
   
+$(document).ready(function() {
+    // Adding clock
+    function displayTime() {
+        var currentTime = new Date();
+	    var hours = currentTime.getHours();
+	    var minutes = currentTime.getMinutes();
+   	    var seconds = currentTime.getSeconds();
+
+            // Grabs the clock id
+            var clockDiv = document.getElementById("clock");
+
+            // Let's set the AM and PM meridiem and 
+            // 12-hour format
+            var meridiem = "AM";  // Default is AM
+
+            // Convert from 24 hour to 12 hour format
+            // and keep track of the meridiem.
+            if (hours > 12) {
+            hours = hours - 12;
+            meridiem = "PM";
+            }
+
+            // 0 AM and 0 PM should read as 12
+            if (hours === 0) {
+            hours = 12;    
+            }
+
+            if (hours < 10) {
+                // Add the "0" digit to the front
+                // so 9 becomes "09"
+                hours = "0" + hours;
+            }
+
+            if (minutes < 10) {
+                // Add the "0" digit to the front
+                // so 9 becomes "09"
+                minutes = "0" + minutes;
+            }
+            
+            // If the seconds digit is less than ten                    
+            if (seconds < 10) {
+                // Add the "0" digit to the front
+                // so 9 becomes "09"
+                seconds = "0" + seconds;
+                }
+
+            // Set text in clock div
+            // Set hours, minutes, and seconds of the current time
+            clockDiv.innerText = hours + ":" + minutes + ":" + seconds + " " + meridiem;             
+}
+
+    // Run displayTime()
+    displayTime();
+
+    // This makes our clock 'tick' by repeatedly
+    // running the displayTime function every second.
+    setInterval(displayTime, 1000);
+
+});
